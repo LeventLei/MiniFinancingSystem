b0965ee2263c4f2fb8db452579ec9cfcb8f1d7e2
LeventLei <443706199@qq.com>
2018-05-24 23:00:40 +0800

    use axios to get first api data

--- src/components/Record.js
+++ src/components/Record.js
@@ -6,9 +6,9 @@ export default class Record extends Component {
 	render() {
 		return (
 			<tr>
-				<td>{this.props.record.date}</td>
-				<td>{this.props.record.title}</td>
-				<td>{this.props.record.ammount}</td>
+				<td>{this.props.date}</td>
+				<td>{this.props.title}</td>
+				<td>{this.props.ammount}</td>
 			</tr>
 		);
 	}
--- src/components/Records.js
+++ src/components/Records.js
@@ -1,34 +1,53 @@
 import React, { Component } from 'react';
 import Record from './Record'
+import axios from 'axios'
 class Records extends Component {
 	constructor() {
 		super();
 		this.state = {
-			records: [
-				{ "id": 1, "date": "2018-05-24", "title": "收入", "ammount": 20 },
-				{ "id": 2, "date": "2018-05-25", "title": "支出", "ammount": 199 },
-				{ "id": 3, "date": "2018-05-26", "title": "支出", "ammount": 1991 }
-			]
+			error: null,
+			records: [],
+			isLoaded: false
 		}
 	}
+	componentDidMount() {
+		axios.get('http://5b068722ff98d70014f3883b.mockapi.io/records').then(
+			response => this.setState({
+				records: response.data,
+				isLoaded: true
+			})
+		).catch(
+			error => this.setState({
+				isLoaded: true,
+				error
+			}))
+	}
 	render() {
-		return (
-			<div>
-				<h2>Records</h2>
-				<table className="table table-bordered">
-					<thead>
-						<tr>
-							<td>Date</td>
-							<td>Title</td>
-							<td>Ammount</td>
-						</tr>
-					</thead>
-					<tbody>
-						{this.state.records.map((record, i) => <Record key={record.id} record={record} />)}
-					</tbody>
-				</table>
-			</div>
-		);
+		const { error, isLoaded, records } = this.state
+		if (error) {
+			return <div>Error: {error.message}</div>;
+		} else if (!isLoaded) {
+			return <div>Loading...</div>;
+		} else {
+			return (
+				<div>
+					<h2>Records</h2>
+					<table className="table table-bordered">
+						<thead>
+							<tr>
+								<td>Date</td>
+								<td>Title</td>
+								<td>Ammount</td>
+							</tr>
+						</thead>
+						<tbody>
+							{records.map((record, i) => <Record key={record.id} {...record} />)}
+						</tbody>
+					</table>
+				</div>
+			);
+		}
+
 	}
 }
 
